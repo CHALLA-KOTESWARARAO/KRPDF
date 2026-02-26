8ab293bdd089597f719c7106b2a0dd9f3f464bb2
kr-1199 <krkoti100@gmail.com>
2026-02-26 13:40:20 +0530

    all

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,23 @@
-﻿import Header from './Header';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import Header from './Header';
 import Footer from './Footer';
 import AdPlaceholder from './AdPlaceholder';
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+  }, [pathname]);
+
+  return null;
+}
 
 function Layout({ children }) {
   return (
     <div className="site-shell">
+      <ScrollToTop />
       <Header />
       <AdPlaceholder type="header" />
       <main>{children}</main>
